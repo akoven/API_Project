d951d3905bd1ed0b8a9754269037ff1758d2b2a1
Attiya <adarensburg@live.com>
2022-04-06 15:38:15 -0500

    completed get routes for tweets path

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/models');
+const asyncHandler = require('./utils');
 
-router.get('/',async(req,res) =>{
-    res.json({message:'test tweets index'});
-})
+const{Tweet} = db;
+const{check,validataionResult} = require('express-validator')
+
+const tweetNotFoundError = (id) => {
+    const error = new Error(`Tweet ${id} was not found`);
+    error.title = 'Tweet not found';
+    error.status = 404;
+    return error;
+}
+
+router.get('/',asyncHandler(async(req,res) =>{
+    // res.json({message:'test tweets index'});
+    const tweets = await Tweet.findAll();
+    res.json({tweets});
+}));
+
+router.get('/:id(\\d+)',asyncHandler(async(req,res,next) => {
+    const id = req.params.id;
+    const foundTweet = await Tweet.findByPk(id);
+    if(!foundTweet){
+        const tweetError = tweetNotFoundError(id)
+        next(tweetError);
+    }else{
+        res.json({foundTweet});
+    }
+}))
+
+
 
 module.exports = router;
